a5c0bd1963c4a5e52f6645d3c9cc5528b70697c7
SIMANT SINGH <124210678+simantsingh5@users.noreply.github.com>
2024-07-20 18:25:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const apiKey = '3576072ad3c9641c760bbe16e7e2639c';  // Replace with your OpenWeatherMap API key
+    const apiKey = '3576072ad3c9641c760bbe16e7e2639c';  // 
     const weatherBtn = document.getElementById('get-weather-btn');
     const cityInput = document.getElementById('city-input');
     const weatherResult = document.getElementById('weather-result');
@@ -50,23 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
         `;
         weatherResult.innerHTML = weatherDetails;
     }
-    // function changeBackgroundImage(temperature) {
-    //     let imageUrl = '';
-
-    //     if (temperature  < 10) {
-    //         imageUrl = url(freezy.jpg);
-    //     } else if (temperature >= 10 && temperature <= 25) {
-    //         imageUrl = 'url(image_url_3)';
-    //     } else if (temperature >= 25 && temperature <= 35) {
-    //         imageUrl = 'url(image_url_4)';
-    //     } else {
-    //         imageUrl = 'url(default_image_url)';
-    //     }
-
-    //     container.style.backgroundImage = imageUrl;
-    // }
-
-    // Trigger getWeather on pressing "Enter" key in the city input field
+   
     cityInput.addEventListener('keydown', (event) => {
         if (event.keyCode === 13) { // Check if "Enter" key is pressed (key code 13)
             event.preventDefault(); // Prevent form submission (to avoid page reload)
